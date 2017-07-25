1b3b4724acb71d1a12b4dd990c9ba8a420406a1b
Christian Heider Nielsen <mrnaah@gmail.com>
2017-07-25 09:52:32 +0200

    Initial Commit

---
+++ Neodroid/Scripts/Messaging/Messages/Reaction.cs
@@ -0,0 +1,32 @@
+﻿using MsgPack.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Neodroid.Messaging.Messages {
+  public class Reaction {
+    public Dictionary<string, MotorMotion> _actor_motor_motions;
+    public bool _reset;
+
+    public Reaction() {
+
+    }
+
+    public Dictionary<string, MotorMotion> GetMotions() {
+      return _actor_motor_motions;
+    }
+
+    public override string ToString() {
+      var text = "";
+      foreach (string key in _actor_motor_motions.Keys) {
+        text += key;
+      }
+
+      foreach (MotorMotion key in _actor_motor_motions.Values) {
+        text += key;
+      }
+      return "<Reaction> " + _reset + ", " + text + " </Reaction>";
+    }
+  }
+}
---
+++ Neodroid/Scripts/Models/Motors/SingleAxisMotor.cs
@@ -0,0 +1,53 @@
+﻿using Neodroid.Messaging.Messages;
+using UnityEngine;
+
+namespace Neodroid.Models.Motors {
+  public enum MotorAxis { X, Y, Z, rot_X, rot_Y, rot_Z, rot_W }
+
+  public class SingleAxisMotor : Motor {
+    public MotorAxis _axis_of_motion;
+
+
+
+    public override void ApplyMotion(MotorMotion motion) {
+      if (_debug) Debug.Log("Applying " + motion._strength.ToString() + " To " + name);
+      if (!_bidirectional && motion._strength < 0) {
+        return; // Do nothing
+      }
+      var new_pos = transform.localPosition;
+      var new_rot = transform.localRotation;
+      switch (_axis_of_motion) {
+        case MotorAxis.X:
+          new_pos.Set(new_pos.x + motion._strength, new_pos.y, new_pos.z);
+          break;
+        case MotorAxis.Y:
+          new_pos.Set(new_pos.x, new_pos.y + motion._strength, new_pos.z);
+          break;
+        case MotorAxis.Z:
+          new_pos.Set(new_pos.x, new_pos.y, new_pos.z + motion._strength);
+          break;
+        case MotorAxis.rot_X:
+          new_rot.Set(new_rot.x + motion._strength, new_rot.y, new_rot.z, new_rot.w);
+          break;
+        case MotorAxis.rot_Y:
+          new_rot.Set(new_rot.x, new_rot.y + motion._strength, new_rot.z, new_rot.w);
+          break;
+        case MotorAxis.rot_Z:
+          new_rot.Set(new_rot.x, new_rot.y, new_rot.z + motion._strength, new_rot.w);
+          break;
+        case MotorAxis.rot_W:
+          new_rot.Set(new_rot.x, new_rot.y, new_rot.z, new_rot.w + motion._strength);
+          break;
+        default:
+          break;
+      }
+      transform.localPosition = new_pos;
+      transform.localRotation = new_rot;
+      _energy_spend_since_reset += _energy_cost * motion._strength;
+    }
+
+    public override string GetMotorIdentifier() {
+      return _axis_of_motion.ToString();
+    }
+  }
+}
