5203efe7bf1c7a5fff1a7b0d322d8b3579f8069f
cnheider <mrnaah@gmail.com>
2017-09-27 14:58:53 +0200

    Ease working with cameras

---
+++ example/Assets/Neodroid/Scripts/Utilities/CameraSynchronisationWindow.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.AnimatedValues;
+
+public class CameraSynchronisationWindow : EditorWindow {
+
+  [MenuItem ("Neodroid/CameraSynchronisationWindow")]
+  public static void ShowWindow () {
+    EditorWindow.GetWindow (typeof(CameraSynchronisationWindow));      //Show existing window instance. If one doesn't exist, make one.
+  }
+
+  SynchroniseCameraProperties[] _cameras;
+  bool[] _show_camera_properties;
+
+  void OnEnable()    {
+    _cameras = FindObjectsOfType<SynchroniseCameraProperties> ();
+    _show_camera_properties = new bool[_cameras.Length];
+    for(int i = 0; i < _cameras.Length; i++){
+      _show_camera_properties[i] = false;
+    }
+  }
+
+  void OnGUI () {
+    SerializedObject serialised_object = new SerializedObject(this);
+    if(_show_camera_properties != null){
+    for(int i = 0; i < _show_camera_properties.Length; i++){
+        _show_camera_properties[i] = EditorGUILayout.Foldout(_show_camera_properties[i],_cameras[i].name);
+        if ( _show_camera_properties[i]){
+          EditorGUILayout.BeginVertical("Box");
+          _cameras[i]._orthographic_size = EditorGUILayout.Toggle ("Synchronise Orthographic Size", _cameras[i]._orthographic_size);
+          _cameras[i]._near_clip_plane = EditorGUILayout.Toggle ("Synchronise Near Clip Plane", _cameras[i]._near_clip_plane);
+          _cameras[i]._far_clip_plane = EditorGUILayout.Toggle ("Synchronise Far Clip Plane", _cameras[i]._far_clip_plane);
+          EditorGUILayout.EndVertical ();
+        }
+    }
+    }
+
+    serialised_object.ApplyModifiedProperties(); // Remember to apply modified properties
+  }
+
+      public void OnInspectorUpdate()
+    {
+        this.Repaint();
+  }
+}
+
---
+++ example/Assets/Neodroid/Scripts/Utilities/SynchroniseCameraProperties.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[RequireComponent(typeof(Camera))]
+[ExecuteInEditMode]
+[Serializable]
+public class SynchroniseCameraProperties : MonoBehaviour {
+
+  public bool _orthographic_size = true;
+  public bool _near_clip_plane = true;
+  public bool _far_clip_plane = true;
+
+  float _old_near_clip_plane;
+  float _old_far_clip_plane;
+  float _old_orthographic_size;
+
+  Camera _camera;
+  Camera[] _cameras;
+
+  public void Start(){
+    _camera = GetComponent<Camera> ();
+    if (_camera) {
+      _old_orthographic_size = _camera.orthographicSize;
+      _old_near_clip_plane = _camera.nearClipPlane;
+      _old_far_clip_plane = _camera.farClipPlane;
+
+      _cameras = FindObjectsOfType<Camera> ();
+    } else {
+      print ("No camera component found on gameobject");
+    }
+
+  }
+
+  public void Update(){
+    if (_camera) {
+      if (_old_orthographic_size != _camera.orthographicSize) {
+        _old_orthographic_size = _camera.orthographicSize;
+        foreach (Camera camera in _cameras) {
+          if (camera != _camera) {
+            camera.orthographicSize = _camera.orthographicSize;
+          }
+        }
+
+      }
+      if (_old_near_clip_plane != _camera.nearClipPlane) {
+        _old_near_clip_plane = _camera.nearClipPlane;
+        foreach (Camera camera in _cameras) {
+          if (camera != _camera) {
+            camera.nearClipPlane = _camera.nearClipPlane;
+          }
+        }
+      }
+      if (_old_far_clip_plane != _camera.farClipPlane) {
+        _old_far_clip_plane = _camera.farClipPlane;
+        foreach (Camera camera in _cameras) {
+          if (camera != _camera) {
+            camera.farClipPlane = _camera.farClipPlane;
+          }
+        }
+      }
+
+    }
+    else {
+      print ("No camera component found on gameobject");
+    }
+
+  }
+}
